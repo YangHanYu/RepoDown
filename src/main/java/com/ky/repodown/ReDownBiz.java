e1d9985f26e5b4a3ef4f363920a416a94e7356de
Hound <Hound@Bumblebee>
2017-06-21 11:50:52 +0800

    为重新下载功能增加水位线限制。并添加布隆过滤去过滤重复的url

--- src/main/java/com/ky/repodown/ReDownBiz.java
+++ src/main/java/com/ky/repodown/ReDownBiz.java
@@ -6,23 +6,44 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
 import com.ky.repodown.common.FtpUtils;
 
 @Component
 @Scope("prototype")
 public class ReDownBiz {
-    
     @Autowired
     private FtpWalker walker;
     
-    public void reDownload(String file) throws UnsupportedEncodingException, FileNotFoundException, IOException{
+    @Autowired
+    private BlockingQueue<Runnable> workQueue;
+    
+    @Autowired
+    private ExecutorService executorService;
+    
+    @Value("${redown.waterLine}")
+    private int workQueueWaterLine = 5000;
+    
+    @Value("${redown.downloadLimit}")
+    private static int downloadLimit = 1000;
+    
+    private Semaphore downloadSemaphore = new Semaphore(downloadLimit);
+    
+    public void reDownload(String file) throws UnsupportedEncodingException, FileNotFoundException, IOException, InterruptedException{
+        BloomFilter<String> bloomFilter = BloomFilter.create((u, ps)->{ps.putString(u, Charsets.UTF_8);}, 1024*1024*32);
+        
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))){
             Pattern urlParrern = Pattern.compile("(?i)(?<=^url:)\\bhttp://[-A-Z0-9+&@#/%?=~_|$!:,.;]*[A-Z0-9+&@#/%=~_|$]");
             
@@ -31,11 +52,27 @@ public class ReDownBiz {
                 Matcher m = urlParrern.matcher(line);
                 if(m.find()){
                     String url = m.group();
+                    
+                    if(bloomFilter.mightContain(url) || url.matches(".+\\.html#?/?$")){
+                        continue;
+                    }
+                    while(workQueue.size()>=workQueueWaterLine){
+                        Thread.sleep(1000);
+                    }
+                    
                     if(FtpUtils.isIndex(url)){
                         walker.walk(url);
                     }else{
-                        walker.download(url);
+                        downloadSemaphore.acquire();
+                        executorService.submit(()->{
+                            try {
+                                walker.download(url);
+                            } finally {
+                                downloadSemaphore.release();
+                            }
+                        });
                     }
+                    bloomFilter.put(url);
                 }
             }
         }
